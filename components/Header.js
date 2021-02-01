af9a091c97e770ccebaa43622a0c3388eadaf15f
Joanne Lee <jokang1005@gmail.com>
2021-02-01 07:54:34 -0600

    setting up switch routes and header componnent in app.js

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Header = (props) => {
+
+    return (
+        <nav>
+            <Link to="/something">Something</Link>
+            
+        </nav>
+    )
+}
+
+export default Header
--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "../components/Header"
+import { Route, Link, Switch } from "react-router-dom"
 //allows this context to be imported someplace else
 export const GlobalCtx = React.createContext(null)
 
-const [gState, setGState] = React.useState({})
+const url = "http://localhost:3000"
+
+const [gState, setGState] = React.useState({url})
 
 function App() {
   return (
     <GlobalCtx.Provider value={{gState, setGState}}>
     <div className="App">
-      
+      <Header/>
+      <main>
+        <Switch>
+          <Route path="/" render= {(rp => <h1>HOME</h1>)}/>
+        </Switch>
+      </main>
     </div>
     </GlobalCtx.Provider>
   );
