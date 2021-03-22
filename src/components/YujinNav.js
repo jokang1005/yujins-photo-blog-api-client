324f8c9a0df1b26bb0b35944cc843015f96208cf
Joanne Lee <jokang1005@gmail.com>
2021-03-22 01:43:00 -0500

    added choose file button

--- src/components/YujinNav.js
+++ src/components/YujinNav.js
@@ -6,17 +6,38 @@ const YujinNav = (props) => {
 
     const {gState, setGState} = React.useContext(GlobalCtx)
 
+    //USE THIS TO POSSIBLY CHANGE THE CODING FROM CLASS TO FUNCTIONAL... BUT NOT SURE HOW YET.
+    // const blank = {
+    //     username: "",
+    //     password: ""
+    // }
 
+    // const [form, setForm] = React.useState(blank)
+
+    // const handleChange = (e) => {
+    //     setForm({...form, [e.target.name]: e.target.value })
+    // }
 
     return(
-        <nav>
-            
-            <Link to="/"><h2 onClick={() => {
-                window.localStorage.removeItem("token")
-                setGState({...gState, token: null})
-            }}>Logout</h2></Link>
+        <>
+            <nav>
+                
+                <Link to="/"><h2 onClick={() => {
+                    window.localStorage.removeItem("token")
+                    setGState({...gState, token: null})
+                }}>Logout</h2></Link>
 
-        </nav>
+            </nav>
+
+            <div className="image-container">
+                <div className="process">
+                    <h4 className="process_heading">Process: Using Multer</h4>
+                    <p className="process_details">Upload image to a node server, connected to a MongoDB database, with the help of Multer</p>
+                    <input type="file" className="process_upload-btn" onChange={(e) => this.uploadImage(e, "multer")}/>
+                    {/* <img src= {this.multerImage} alt= "upload-image" className="process_image"/> */}
+                </div>
+            </div>
+        </>
     )
 }
 
